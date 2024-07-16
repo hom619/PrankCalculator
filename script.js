5c1bd90bbfe4d950c36c82c82d572472202256ae
hom619 <hom.grg619@gmail.com>
2024-07-16 13:56:45 +1000

    Calculate the total using = button

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const allButtons = document.querySelectorAll(".btn"); //accessing all the button
 // console.log(allButtons);
 const ACbuttonElm = document.querySelector(".btn-AC");
 const displayElm = document.querySelector(".topdisplay");
+const equalElm = document.querySelector(".btn-equal");
 let numberToDisplay = "";
 const buttonAction = (value) => {
   //created a separte function for the button action inorder to minimise the code in the loop
   if (value === ACbuttonElm.innerText) {
     numberToDisplay = "";
     return display(numberToDisplay);
+  }
+  if (value === equalElm.innerText) {
+    return calculateTotal();
   }
   numberToDisplay += value;
   display(numberToDisplay);
@@ -17,10 +21,14 @@ allButtons.forEach((btn) => {
     //here the event listener will create an event and when it activates it will run the given code
     const value = btn.innerText; //this recieves the inner text of each button when clicked
     buttonAction(value);
-    // console.log(numberToDisplay);
   });
 });
 
 const display = (str) => {
   displayElm.innerText = str || "0.0";
+};
+
+const calculateTotal = () => {
+  const total = eval(numberToDisplay); //eval function will evaluate the string expression and makes the calculation
+  display(total);
 };
