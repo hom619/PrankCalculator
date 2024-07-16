1137e0d8b80a86780ec297e812c6b0f14877c130
hom619 <hom.grg619@gmail.com>
2024-07-16 13:30:11 +1000

    The string captured from each button click are displayed in the string and made the AC button work which clears the screen

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const allButtons = document.querySelectorAll(".btn"); //accessing all the buttons
 // console.log(allButtons);
-
+const ACbuttonElm = document.querySelector(".btn-AC");
+const displayElm = document.querySelector(".topdisplay");
 let numberToDisplay = "";
+const buttonAction = (value) => {
+  //created a separte function for the button action inorder to minimise the code in the loop
+  if (value === ACbuttonElm.innerText) {
+    numberToDisplay = "";
+    return display(numberToDisplay);
+  }
+  numberToDisplay += value;
+  display(numberToDisplay);
+};
 allButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     //here the event listener will create an event and when it activates it will run the given code
     const value = btn.innerText; //this recieves the inner text of each button when clicked
-    numberToDisplay += value;
+    buttonAction(value);
     // console.log(numberToDisplay);
   });
 });
+
+const display = (str) => {
+  displayElm.innerText = str || "0.0";
+};
