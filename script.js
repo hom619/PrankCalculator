0d2989b41da40d9672cc65ae2343536a6e66fcf6
hom619 <hom.grg619@gmail.com>
2024-07-16 17:25:25 +1000

    Handled multiple operators as well as single operator at the end

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const displayElm = document.querySelector(".topdisplay");
 const equalElm = document.querySelector(".btn-equal");
 const cutElm = document.querySelector(".btn-C");
 let numberToDisplay = "";
+const operators = ["+", "-", "%", "/", "*"];
+const checkLastOperator = () => {
+  const lastChar = numberToDisplay[numberToDisplay.length - 1];
+  if (operators.includes(lastChar)) {
+    //this will check if there is any operator at the last and if there is then it will ignore the last operator and do the calculation
+    numberToDisplay = numberToDisplay.slice(0, -1);
+  }
+};
 const buttonAction = (value) => {
   //created a separte function for the button action inorder to minimise the code in the loop
   if (value === ACbuttonElm.innerText) {
@@ -12,11 +20,15 @@ const buttonAction = (value) => {
     return display(numberToDisplay);
   }
   if (value === equalElm.innerText) {
+    checkLastOperator();
     return calculateTotal();
   }
   if (value === cutElm.innerText) {
     numberToDisplay = numberToDisplay.slice(0, -1); //here the slice method will read the string from index 0 and stops before the last character
     return display(numberToDisplay);
+  }
+  if (operators.includes(value)) {
+    checkLastOperator();
   }
   numberToDisplay += value;
   display(numberToDisplay);
