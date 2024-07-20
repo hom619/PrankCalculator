3b11f45c24ae58a39b142fa068621f50c7c2f3c3
hom619 <hom.grg619@gmail.com>
2024-07-20 18:58:33 +1000

    Added button push effect on each button click and added animation while loading the calculator

--- script.js
+++ script.js
@@ -50,8 +50,12 @@ const buttonAction = (value) => {
   display(numberToDisplay);
 };
 allButtons.forEach((btn) => {
+  btn.addEventListener("mousedown", () => {
+    btn.style.scale = ".9";
+  });
   btn.addEventListener("click", () => {
     //here the event listener will create an event and when it activates it will run the given code
+    btn.style.scale = "1";
     const value = btn.innerText; //this recieves the inner text of each button when clicked
     buttonAction(value);
   });
